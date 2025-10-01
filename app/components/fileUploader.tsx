831f9db58a265d76e5ab7c1aafffcd27fc9c27f0
Aditya Kumar Gupta <aditya.gupta@nzminds.com>
2025-10-01 22:20:02 +0530

    changes for file upload

--- app/components/fileUploader.tsx
+++ app/components/fileUploader.tsx
@@ -7,36 +7,59 @@ interface FileUploaderProps {
 }
 
 const FileUploader = ({ onFileSelect }: FileUploaderProps) => {
-  const onDrop = useCallback((acceptedFiles: File[]) => {
-    const file = acceptedFiles[0] || null;
-    onFileSelect?.(file);
-  }, [onFileSelect]);
+  const onDrop = useCallback(
+    (acceptedFiles: File[]) => {
+      const file = acceptedFiles[0] || null;
+      onFileSelect?.(file);
+    },
+    [onFileSelect]
+  );
   const maxFileSize = 20 * 1024 * 1024; // 20MB
-  const { getRootProps, getInputProps, isDragActive, acceptedFiles } = useDropzone({ onDrop, multiple: false, maxSize: maxFileSize, accept: { 'application/pdf': ['.pdf'] } });
+  const { getRootProps, getInputProps, isDragActive, acceptedFiles } =
+    useDropzone({
+      onDrop,
+      multiple: false,
+      maxSize: maxFileSize,
+      accept: { "application/pdf": [".pdf"] },
+    });
   const file = acceptedFiles[0];
-  
+
   return (
     <div className="w-full gradient-border">
       <div {...getRootProps()}>
         <input {...getInputProps()} />
         <div className="space-y-4 cursor-pointer">
-          <div className="mx-auto w-16 h-16 flex items-center justify-center">
-            <img src="/icons/info.svg" alt="upload" className="size-20" />
-          </div>
           {file ? (
-            <div className="text-center">
-                <p className="text-lg text-gray-700 font-medium truncate">
+            <div
+              className="uploader-selected-file"
+              onClick={(e) => {
+                e.stopPropagation();
+              }}
+            >
+              <img src="/images/pdf.png" alt="pdf" className="size-10" />
+              <div className="flex items-center space-x-3">
+                <div>
+                  <p className="text-sm text-gray-700 font-medium truncate max-w-xs">
                     {file.name}
-                </p>
-                <p className="text-sm text-gray-500">{formatSize(file.size)}</p>
+                  </p>
+                  <p className="text-sm text-gray-500">
+                    {formatSize(file.size)}
+                  </p>
+                </div>
+              </div>
             </div>
           ) : (
             <div>
+              <div className="mx-auto w-16 h-16 flex items-center justify-center mb-2">
+                <img src="/icons/info.svg" alt="upload" className="size-20" />
+              </div>
               <p className="text-lg text-gray-500">
                 <span className="font-semibold">Click to Upload</span> or drag
                 and drop
               </p>
-              <p className="text-lg text-gray-500">PDF (max {formatSize(maxFileSize)}) </p>
+              <p className="text-lg text-gray-500">
+                PDF (max {formatSize(maxFileSize)}){" "}
+              </p>
             </div>
           )}
         </div>
