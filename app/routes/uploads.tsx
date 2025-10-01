f0cd24a8097c2d2561b33024febb94ac4655476a
Aditya Kumar Gupta <aditya.gupta@nzminds.com>
2025-10-01 22:06:46 +0530

    changes for file upload

---
+++ app/routes/uploads.tsx
@@ -0,0 +1,80 @@
+import Navbar from "~/components/navbar";
+import { useState, type FormEvent } from "react";
+import { isPromise } from "util/types";
+import FileUploader from "~/components/fileUploader";
+
+const Upload = () => {
+  const [isProcessing, setIsProcessing] = useState(false);
+  const [statusText, setStatusText] = useState("");
+  const [file, setFile] = useState<File | null>(null);
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {};
+  const handleFileSelect = (file: File) => {
+    setFile(file);
+  };
+
+  
+  return (
+    <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+      <Navbar />
+      <section className="main-section">
+        <div className="page-heading py-16">
+          <h1>Smart feedback for your dream job.</h1>
+          {isProcessing ? (
+            <>
+              <h2>{statusText}</h2>
+              <img src="/images/resume-scan.gif" className="w-full" />
+            </>
+          ) : (
+            <>
+              <h2>Drop your resume for an ATS score and improvement tips</h2>
+            </>
+          )}
+          {!isProcessing && (
+            <form
+              id="upload-form"
+              className="w-full flex flex-col gap-4 mt-8"
+              onSubmit={handleSubmit}
+            >
+              <div className="form-div">
+                <label htmlFor="company-name">Company Name</label>
+                <input
+                  type="text"
+                  id="company-name"
+                  name="company-name"
+                  placeholder="Enter company name"
+                />
+              </div>
+              <div className="form-div">
+                <label htmlFor="job-title">Job Title</label>
+                <input
+                  type="text"
+                  id="job-title"
+                  name="job-title"
+                  placeholder="Enter job title"
+                />
+              </div>
+              <div className="form-div">
+                <label htmlFor="job-description">Job Description</label>
+                <textarea
+                  rows={5}
+                  id="job-description"
+                  name="job-description"
+                  placeholder="Enter job description"
+                />
+              </div>
+              <div className="form-div">
+                <label htmlFor="uploader">Upload Resume</label>
+                <FileUploader onFileSelect={handleFileSelect} />
+              </div>
+              <button className="primary-button" type="submit">
+                Analyze Resume
+              </button>
+            </form>
+          )}
+        </div>
+      </section>
+    </main>
+  );
+};
+
+export default Upload;
